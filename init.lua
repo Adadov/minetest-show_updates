a0ace4acc9313616a89af8cdea5d9edb87629cca
David OLIVIER <adadov@adadov.net>
2017-09-07 12:32:19 +0200

    Optimisation

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
-if io.open(minetest.get_worldpath().."/modifications.txt", "r") then
-	local f = assert(io.open(minetest.get_worldpath().."/modifications.txt", "r"))
-	local the_text = f:read("*all")
+local input = io.open(minetest.get_worldpath().."/modifications.txt", "r")
+if input then
+	local the_text = input:read("*all")
 	f:close()
-	
+
 	minetest.register_on_joinplayer(function(player)
-	
+
 		minetest.show_formspec(player:get_player_name(), "start_screen",
 			"size[12,8;]"..
 			"bgcolor[#73767B;false]"..
 			"textarea[0.5,0.5;11.5,8;name;;"..the_text.."]"..
 			"button_exit[5,7.5;6,2;yes;OK]");
-	
+
 	end)
 end
