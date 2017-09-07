cd73bee2e984743c1656d4aaab856c49746ce0bd
David OLIVIER <adadov@adadov.net>
2017-09-07 12:29:04 +0200

    Ajout vérification de présence du fichier txt

--- init.lua
+++ init.lua
@@ -1,13 +1,15 @@
-local f = assert(io.open(minetest.get_worldpath().."/modifications.txt", "r"))
-local the_text = f:read("*all")
-f:close()
-
-minetest.register_on_joinplayer(function(player)
-
-	minetest.show_formspec(player:get_player_name(), "start_screen",
-		"size[12,8;]"..
-		"bgcolor[#73767B;false]"..
-		"textarea[0.5,0.5;11.5,8;name;;"..the_text.."]"..
-		"button_exit[5,7.5;6,2;yes;OK]");
-
-end)
+if io.open(minetest.get_worldpath().."/modifications.txt", "r") then
+	local f = assert(io.open(minetest.get_worldpath().."/modifications.txt", "r"))
+	local the_text = f:read("*all")
+	f:close()
+	
+	minetest.register_on_joinplayer(function(player)
+	
+		minetest.show_formspec(player:get_player_name(), "start_screen",
+			"size[12,8;]"..
+			"bgcolor[#73767B;false]"..
+			"textarea[0.5,0.5;11.5,8;name;;"..the_text.."]"..
+			"button_exit[5,7.5;6,2;yes;OK]");
+	
+	end)
+end
